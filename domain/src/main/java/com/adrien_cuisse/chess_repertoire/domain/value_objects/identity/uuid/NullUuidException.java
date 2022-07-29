adad70b189390ce021b9ab0e38624dd187645bba
Adrien Cuisse <adrien.cuisse@epitech.eu>
2022-07-29 05:18:43 +0200

    exceptions (missing from previous commit)

---
+++ domain/src/main/java/com/adrien_cuisse/chess_repertoire/domain/value_objects/identity/uuid/NullUuidException.java
@@ -0,0 +1,10 @@
+
+package com.adrien_cuisse.chess_repertoire.domain.value_objects.identity.uuid;
+
+public final class NullUuidException extends RuntimeException
+{
+	public NullUuidException()
+	{
+		super("Uuid is null");
+	}
+}
